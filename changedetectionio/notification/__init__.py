d626b22f4879e40f3a3a8f67ba5fda4a0265d42f
radoinrara-commits <radoinrara@gmail.com>
2026-03-31 23:34:51 +0100

    Update __init__.py

--- changedetectionio/notification/__init__.py
+++ changedetectionio/notification/__init__.py
@@ -1,8 +1,8 @@
 from changedetectionio.model import USE_SYSTEM_DEFAULT_NOTIFICATION_FORMAT_FOR_WATCH
 
-default_notification_format = 'htmlcolor'
-default_notification_body = '{{watch_url}} had a change.\n---\n{{diff}}\n---\n'
-default_notification_title = 'ChangeDetection.io Notification - {{watch_url}}'
+default_notification_format = 'text'
+default_notification_body = '\n---\n{{diff}}\n---\n'
+default_notification_title = 'ChangeDetection - {{watch_title}}'
 
 # The values (markdown etc) are from apprise NotifyFormat,
 # But to avoid importing the whole heavy module just use the same strings here.
